56b35a4d9862b9905d449e5a4f03b6349c6648b4
Leo Kaluzhny <leokaluzhny@gmail.com>
2025-05-06 14:25:38 -0400

    added linear, hybrid for nll, hybrid for mse/mae

---
+++ model/regression_model.py
@@ -0,0 +1,31 @@
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
+
+N_ALT = 10
+N_LAT = 16
+N_LON = 16
+
+NUM_GRID_FEATURES = N_ALT * N_LAT * N_LAT # Number of grid features
+NUM_LINEAR_FEATURES = 4 # lat long alt delta_t 
+NUM_TOTAL_FEATURES = NUM_GRID_FEATURES + NUM_LINEAR_FEATURES
+NUM_FIELDS = 1
+
+class LinearClassifierModel(nn.Module):
+    def __init__(self):
+        super(LinearClassifierModel, self).__init__()
+        # Set a random seed for reproducibility
+        torch.manual_seed(42)  # Ensures weight initialization is the same each run
+        torch.cuda.manual_seed_all(42)  
+        output_len = 10
+        self.linear = nn.Linear(NUM_TOTAL_FEATURES, output_len)
+
+    def forward(self, x):
+        # print(f"forward called with inputs {x}")
+        return self.linear(x)
+    
+
+
+
+
