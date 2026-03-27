5dfa5478125579d713afe206f8f7f367e9724878
eelkus01 <eelkus01@pax052.pax.tufts.edu>
2026-03-27 13:24:59 -0400

    small style fix for model results printing

--- model_training/train_and_test_model.py
+++ model_training/train_and_test_model.py
@@ -369,13 +369,18 @@ def main():
             if true_label > 0 and predicted_class == 0:
                 num_false_negative += 1
 
-    # Output results to the designated results file
-    RESULTS_FILE.write(f"For the test data, the actual distribution of classes are: {actual_output_counts}")
-    RESULTS_FILE.write(f"For the test data, the model's distribution of classes are: {model_output_counts}")
-
-    RESULTS_FILE.write(f"Accuracy is: {num_correct}/{len(test_dataset)}, or {num_correct/len(test_dataset) * 100}%")
-    RESULTS_FILE.write(f"The false positive rate is: {num_false_positive}/{len(test_dataset)}, or {num_false_positive/len(test_dataset) * 100}%")
-    RESULTS_FILE.write(f"The false negative rate is: {num_false_negative}/{len(test_dataset)}, or {num_false_negative/len(test_dataset) * 100}%")
+    # Output results to the designated results file and job output file
+    RESULTS_FILE.write(f"For the test data, the actual distribution of classes are: {actual_output_counts}\n")
+    RESULTS_FILE.write(f"For the test data, the model's distribution of classes are: {model_output_counts}\n")
+    RESULTS_FILE.write(f"Accuracy is: {num_correct}/{len(test_dataset)}, or {num_correct/len(test_dataset) * 100}%\n")
+    RESULTS_FILE.write(f"The false positive rate is: {num_false_positive}/{len(test_dataset)}, or {num_false_positive/len(test_dataset) * 100}%\n")
+    RESULTS_FILE.write(f"The false negative rate is: {num_false_negative}/{len(test_dataset)}, or {num_false_negative/len(test_dataset) * 100}%\n")
+    
+    print(f"For the test data, the actual distribution of classes are: {actual_output_counts}")
+    print(f"For the test data, the model's distribution of classes are: {model_output_counts}")
+    print(f"Accuracy is: {num_correct}/{len(test_dataset)}, or {num_correct/len(test_dataset) * 100}%")
+    print(f"The false positive rate is: {num_false_positive}/{len(test_dataset)}, or {num_false_positive/len(test_dataset) * 100}%")
+    print(f"The false negative rate is: {num_false_negative}/{len(test_dataset)}, or {num_false_negative/len(test_dataset) * 100}%")
     
 
     # Save the best model
